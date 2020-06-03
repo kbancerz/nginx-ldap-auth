3f27bc552eaeed4e8cbc707b0f6576d46a0d31ce
Kajetan Bancerz <kajetan.bancerz@gmail.com>
2020-06-03 11:26:03 +0200

    Implement new configuration consistency check

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DEFAULT_SESSION_EXPIRATION = 86400  # session lasts 1 day by default
 GROUPS_CACHE_MAX_AGE = 300  # cache user's groups query for 5 minutes
 
 CONFIG_TEMPLATE = {
-    'basic_authentication': {
+    'basic': {
         'enabled': False,
         'users': {
             'sample_user': {
@@ -55,14 +55,14 @@ class Config(object):
     def __init__(self, config_data):
         self._config_dict = json.loads(config_data)
 
-        basic = self._config_dict.get('basic_authentication', {})
+        basic = self._config_dict.get('basic', {})
         ldap = self._config_dict.get('ldap', {})
         session = self._config_dict.get('session', {})
         pages = self._config_dict.get('pages', {})
         ingress = self._config_dict.get('ingress', {})
 
         self.basic_enabled = basic.get('enabled', False)
-        self.basic_users = basic.get('users', None)
+        self.basic_users = basic.get('users', {})
 
         self.ldap_enabled = ldap.get('enabled', False)
         self.host = ldap.get('host', None)
@@ -86,6 +86,63 @@ class Config(object):
         self.fallback_redirect = pages.get('fallback_redirect', None)
 
         self.ignored_addresses = ingress.get('ignored_addresses', None)
+
+    def check_consistency(self):
+        invalid = []
+
+        # check Basic authentication configuration
+        if self.basic_enabled:
+            if not self.basic_users:
+                invalid.append('basic.users')
+            elif not isinstance(self.basic_users, dict):
+                invalid.append('basic.users')
+            else:
+                for user, data in self.basic_users.items():
+                    if not isinstance(data.get('password', None), str):
+                        invalid.append(f'basic.users.{user}.password')
+                    if not isinstance(data.get('groups', None), list):
+                        invalid.append(f'basic.users.{user}.groups')
+
+        # check LDAP authentication configuration
+        if self.ldap_enabled:
+            if self.host is None:
+                invalid.append('ldap.host')
+            if self.port is None:
+                invalid.append('ldap.port')
+            if self.username is None:
+                invalid.append('ldap.username')
+            if self.password is None:
+                invalid.append('ldap.password')
+            if self.user_attr is None:
+                invalid.append('ldap.user_attr')
+            if self.group_base_dn is None:
+                invalid.append('ldap.group_base_dn')
+            if self.group_cache_max_age is None:
+                invalid.append('ldap.group_cache_max_age')
+
+        # check HTTP session configuration
+        if self.cookie_session is None:
+            invalid.append('session.cookie_session')
+        if self.cookie_redirect is None:
+            invalid.append('session.cookie_redirect')
+        if self.cookie_domain is None:
+            invalid.append('session.cookie_domain')
+        if self.cookie_secret is None:
+            invalid.append('session.cookie_secret')
+        if self.cookie_session is None:
+            invalid.append('session.cookie_session')
+
+        # check pages configuration
+        if self.login_template is None:
+            invalid.append('pages.login_template')
+        if self.noauth_template is None:
+            invalid.append('pages.noauth_template')
+        if self.static_root is None:
+            invalid.append('pages.static_root')
+        if self.fallback_redirect is None:
+            invalid.append('pages.fallback_redirect')
+
+        return invalid
 
     @staticmethod
     def get_sample_config(indent):
